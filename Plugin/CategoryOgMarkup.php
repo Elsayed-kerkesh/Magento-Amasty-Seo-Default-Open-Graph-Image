99dc0841d751850cc8571c21f12bb1b17ee8cd61
Sayed Mohamed <pro4soft10@gmail.com>
2024-06-30 00:31:04 +0300

    initial comment

---
+++ Plugin/CategoryOgMarkup.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) 2024  @ Tirador Company
+ * developer :  Sayed Mohamed
+ */
+
+namespace Tecno\AmastyOpenGraphTags\Plugin;
+
+/**
+ * Class CategoryOgMarkup
+ *
+ * @package \Tirador\AmastyOpenGraphTags\Plugin
+ */
+class CategoryOgMarkup
+{
+
+    public function afterGetCategoryImageUrl(\Amasty\OpenGraphTags\ViewModel\CategoryOgMarkup $subject, $result)
+    {
+        if (!$result) {
+
+        }
+    }
+}
---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 2024  @ Tirador Company
+ * developer :  Sayed Mohamed
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Tecno_AmastyOpenGraphTags', __DIR__);
